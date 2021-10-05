aff9c61a98fba86e50951819329a6f8684ec3a81
m66rk9t <m66rk9t@gmail.com>
2021-10-05 20:59:45 +0800

    使用顺序栈

--- 数据结构（C语言版）/03-顺序栈/usestack.c
+++ 数据结构（C语言版）/03-顺序栈/usestack.c
@@ -10,14 +10,15 @@ int main(void)
     Elem input, reminder, temp; //输入的值、余数以及临时变量
     Elem *pElem;                //指向栈元素的指针
     InitStack(&ss);             //初始化栈
-
-    /*获取输入*/
+    
+    //获取输入
     printf("Please enter an integer number:");
     while (scanf("%d", &input) != 1 || input <= 0)
         printf("Invalid! Enter again:");
     temp = input;
 
-    /*入栈*/
+
+    //入栈
     while (input)
     {
         reminder = input % 2;
@@ -25,18 +26,29 @@ int main(void)
         input = input / 2;
     }
 
-    /*出栈*/
-    printf("%d to binary:", temp);
-    pElem = ss.top;
-    while (--pElem >= ss.base)
-    {
-        Pop(&ss, &reminder);
-        printf("%d", reminder);
-    }
-    putchar('\n');
+    //出栈一个元素
+    Pop(&ss, &reminder);
+    printf("%d\n", reminder);
+    printf("%u\n", StackLength(&ss));
 
-    /*销毁*/
-    DestoryStack(&ss);
+    //出栈
+    // printf("%d to binary:", temp);
+    // pElem = ss.top;
+    // while (--pElem >= ss.base)
+    // {
+    //     Pop(&ss, &reminder);
+    //     printf("%d", reminder);
+    // }
+    // putchar('\n');
+
+    // 销毁
+    if (DestoryStack(&ss))
+        printf("Destoryed successfully.\n");
+    if (!DestoryStack(&ss))
+        printf("Stack has been destoryed.\n");
+    if (StackIsNull(&ss))
+        printf("Empty.\n");
+    
 
     return 0;
 }
