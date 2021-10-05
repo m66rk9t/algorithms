b48b25fe3bd2a61b9e05dc08a8393d3c2fcbaf01
m66rk9t <m66rk9t@gmail.com>
2021-10-05 09:41:52 +0800

    sb键盘

--- 数据结构（C语言版）/03-顺序栈/usestack.c
+++ 数据结构（C语言版）/03-顺序栈/usestack.c
@@ -7,28 +7,9 @@
 int main(void)
 {
     SeqStack ss;
-    Elem input, reminder;
+    InitStack(&ss);    //初始化栈
 
-    InitStack(&ss); //初始化栈
-
-    /*获取输入*/
-    printf("Please enter an integer number:");
-    while (scanf("%d", &input) != 1 || input <= 0)
-        printf("Invalid! Enter again:");
-    
-    /*转换进制，入栈*/
-    while (input)
-    {
-        reminder = input % 2;
-        Push(&ss, &reminder);
-        input = input / 2;
-    }
-
-    /*出栈，打印数据*/
-    StackTraverse(&ss, PrintElemData);
-    
-    /*销毁栈*/
-    DestoryStack(&ss);
+    DestoryStack(&ss); //销毁栈
 
     return 0;
 }
