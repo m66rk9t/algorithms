af38ab2f81942aabce349f700548a9a4d19458c6
m66rk9t <m66rk9t@gmail.com>
2021-10-05 11:48:58 +0800

    使用顺序栈

--- 数据结构（C语言版）/03-顺序栈/usestack.c
+++ 数据结构（C语言版）/03-顺序栈/usestack.c
@@ -6,10 +6,37 @@
 
 int main(void)
 {
-    SeqStack ss;
-    InitStack(&ss);    //初始化栈
+    SeqStack ss;                //声明一个栈
+    Elem input, reminder, temp; //输入的值、余数以及临时变量
+    Elem *pElem;                //指向栈元素的指针
+    InitStack(&ss);             //初始化栈
 
-    DestoryStack(&ss); //销毁栈
+    /*获取输入*/
+    printf("Please enter an integer number:");
+    while (scanf("%d", &input) != 1 || input <= 0)
+        printf("Invalid! Enter again:");
+    temp = input;
+
+    /*入栈*/
+    while (input)
+    {
+        reminder = input % 2;
+        Push(&ss, &reminder);
+        input = input / 2;
+    }
+
+    /*出栈*/
+    printf("%d to binary:", temp);
+    pElem = ss.top;
+    while (--pElem >= ss.base)
+    {
+        Pop(&ss, &reminder);
+        printf("%d", reminder);
+    }
+    putchar('\n');
+
+    /*销毁*/
+    DestoryStack(&ss);
 
     return 0;
 }
