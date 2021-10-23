b4adaa18b5b3865dedad05e6e777f6d7c0a0fa0d
m66rk9t <m66rk9t@gmail.com>
2021-10-23 11:48:42 +0800

    The definition of sequential queue

--- C Language Version by YanWeimin/03-SeqQueue/seqqueue.h
+++ C Language Version by YanWeimin/03-SeqQueue/seqqueue.h
@@ -9,11 +9,11 @@ typedef int QElem;  //队列元素
 #define MAXQSIZE 11 //队列大小
 typedef struct seqqueue
 {
-    QElem *base; //指向动态分配内存
-    int head;    //指向队头
-    int tail;    //指向队尾的下一个位置
-    int maxqsize;//队列大小
-} SeqQueue;      //队列结构
+    QElem *base;  //指向动态分配内存
+    int head;     //指向队头
+    int tail;     //指向队尾的下一个位置
+    int maxqsize; //队列大小
+} SeqQueue;       //队列结构
 
 /*函数原型*/
 /*操作：初始化队列*/
@@ -21,6 +21,10 @@ typedef struct seqqueue
 /*后置条件：将sq指向的队列初始化为空队列*/
 /*         初始化成功返回true，否则返回false*/
 bool InitQueue(SeqQueue *sq);
+/*操作：销毁队列*/
+/*前置条件：sq指向一个已初始化的队列*/
+/*后置条件：释放sq指向的队列的存储空间*/
+void DestoryQueue(SeqQueue *sq);
 /*操作：判断队列是否为空*/
 /*前置条件：sq指向一个已初始化的队列*/
 /*后置条件：sq指向的队列为空则返回true，否则返回false*/
@@ -43,6 +47,11 @@ bool EnQueue(SeqQueue *sq, QElem *elem);
 /*后置条件：将队头元素出队，并用elem返回其值*/
 /*         出队成功返回true，否则返回false*/
 bool DeQueue(SeqQueue *sq, QElem *elem);
+/*操作：读取队头元素值*/
+/*前置条件：sq指向一个已初始化且不为空的队列*/
+/*后置条件：读取队头元素的值，并用elem返回*/
+/*         读取成功返回true，否则返回false*/
+bool GetQHead(SeqQueue *sq, QElem *elem);
 /*操作：打印队列元素值*/
 /*前置条件：elem指向队列中的元素*/
 /*后置条件：打印elem指向的元素值*/
@@ -50,6 +59,6 @@ void PrintQElem(QElem *elem);
 /*操作：遍历队列*/
 /*前置条件：sq指向一个已初始化的队列*/
 /*后置条件：将pFun指向的函数作用于队列中的每个元素*/
-void QueueTraverse(SeqQueue *sq, void(*pFun)(QElem *));
+void QueueTraverse(SeqQueue *sq, void (*pFun)(QElem *));
 
 #endif
