7a45d615b3780f0bc826584c9b4ecbeb9a436f68
m66rk9t <m66rk9t@gmail.com>
2021-10-23 11:34:09 +0800

    use sequential queue

---
+++ C Language Version by YanWeimin/03-SeqQueue/usequeue.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "seqqueue.h"
+
+int main(void)
+{
+    SeqQueue sq;
+    QElem elem = 1;
+    InitQueue(&sq);
+    for (int i = 0; i < sq.maxqsize; i++, elem++)
+    {
+        EnQueue(&sq, &elem);
+    }
+    QueueTraverse(&sq, PrintQElem);
+
+
+
+    return 0;
+}
