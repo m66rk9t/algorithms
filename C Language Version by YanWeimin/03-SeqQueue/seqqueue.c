261430a20b7cfc23a09c23bd21605198d76f9078
m66rk9t <m66rk9t@gmail.com>
2021-11-05 19:31:47 +0800

    update the implementation of sequential queue

--- C Language Version by YanWeimin/03-SeqQueue/seqqueue.c
+++ C Language Version by YanWeimin/03-SeqQueue/seqqueue.c
@@ -1,38 +1,36 @@
-/*队列的顺序存储实现*/
-#include <stdbool.h>
+/*顺序队列实现*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "seqqueue.h"
 
-/*函数定义*/
 bool InitQueue(SeqQueue *sq)
 {
-    //为队列分配存储空间
-    sq->base = (QElem *)malloc(MAXQSIZE * sizeof(QElem));
+    /*为队列分配存储空间*/
+    sq->base = (QElem *)malloc(INITQSIZE * sizeof(QElem));
 
-    //分配失败，返回false，初始化失败
-    if (sq->base == NULL)
+    /*分配失败返回false*/
+    if (!sq->base)
         return false;
-    
-    //分配成功，设置队头队尾
+
+    /*设置头、尾指针以及最大长度*/
     sq->head = sq->tail = 0;
-    
-    //设置队列大小
-    sq->maxqsize = MAXQSIZE - 1;
-    
-    //初始化成功
+    sq->maxqsize = INITQSIZE - 1;
+
+    /*初始化成功*/
     return true;
 }
 
 void DestoryQueue(SeqQueue *sq)
 {
+    /*释放动态内存*/
     free(sq->base);
 }
 
 bool QueueIsEmpty(SeqQueue *sq)
 {
-    //队列长度不为0，队列不为空
-    if (!QueueLength(sq))
+    /*当队头队尾指针相等时，队列为空*/
+    if (sq->head == sq->tail)
         return true;
     else
         return false;
@@ -40,69 +38,69 @@ bool QueueIsEmpty(SeqQueue *sq)
 
 bool QueueIsFull(SeqQueue *sq)
 {
-    //队列长度未达到上限
-    if (QueueLength(sq) == sq->maxqsize)
+    /*当队尾的下一个元素指针与队头指针相等时，队列已满*/
+    if ((sq->tail + 1) % INITQSIZE == sq->head)
         return true;
     else
         return false;
 }
 
 int QueueLength(SeqQueue *sq)
-{
-    //返回队列长度
-    return (sq->tail - sq->head);
+{   
+    /*计算并返回队列中元素数量*/
+    return (((sq->tail - sq->head) + INITQSIZE) % INITQSIZE);
 }
 
 bool EnQueue(SeqQueue *sq, QElem *elem)
 {
-    //队列已满，入队失败
+    /*队满，无法入队*/
     if (QueueIsFull(sq))
         return false;
-
-    //入队，重置队尾指针
+    
+    /*元素入队，重置队头指针*/
     sq->base[sq->tail] = *elem;
-    sq->tail += 1;
+    sq->tail = (sq->tail + 1) % INITQSIZE;
 
-    //入队成功
+    /*入队成功*/
     return true;
 }
 
 bool DeQueue(SeqQueue *sq, QElem *elem)
 {
-    //队列为空，出队失败
+    /*队空，无法出队*/
     if (QueueIsEmpty(sq))
         return false;
 
-    //出队，重置队头指针
+    /*元素出队，重置队尾指针*/
     *elem = sq->base[sq->head];
-    sq->head += 1;
+    sq->head = (sq->head + 1) % INITQSIZE;
 
-    //出队成功
+    /*出队成功*/
     return true;
 }
 
 bool GetQHead(SeqQueue *sq, QElem *elem)
 {
-    //队列为空，读取失败
+    /*队空，无法访问队头元素*/
     if (QueueIsEmpty(sq))
         return false;
 
-    //读取队头元素值
+    /*访问队头元素*/
     *elem = sq->base[sq->head];
 
-    //读取队头元素值成功
+    /*访问成功*/
     return true;
 }
 
 void PrintQElem(QElem *elem)
 {
-    //打印队列元素值
-    printf("%d\n", *elem);
+    /*打印队列元素值*/
+    printf("%-2d", *elem);
 }
 
 void QueueTraverse(SeqQueue *sq, void (*pFun)(QElem *))
 {
-    //遍历队列
+    /*遍历队列，并用pFun指向的函数作用于每个队列元素*/
     for (int i = 0; i < QueueLength(sq); i++)
         pFun(&(sq->base[i]));
 }
