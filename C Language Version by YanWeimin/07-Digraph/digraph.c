801101d155ee5a2bf5039fb3700fc5553a58d1a3
m66rk9t <m66rk9t@gmail.com>
2021-11-07 19:54:08 +0800

    update the implementation of digraph

--- C Language Version by YanWeimin/07-Digraph/digraph.c
+++ C Language Version by YanWeimin/07-Digraph/digraph.c
@@ -18,13 +18,15 @@ bool CreateGraph(Digraph *dg)
         continue;
 
     /*输入顶点信息并初始化顶点数组*/
+    printf("Please enter a character as the symbol of vertex: ");
     for (int i = 0; i < dg->vers; i++)
     {
-        printf("Please enter a character as the symbol of vertex: ");
         dg->vertices[i].elem = getchar();
         /*清理输入行*/
         while (getchar() != '\n')
             continue;
+        if (i != 9)
+            printf("Enter the next character: ");
     }
 
     /*初始化邻接矩阵*/
@@ -39,7 +41,8 @@ bool CreateGraph(Digraph *dg)
         scanf("%d,%d", &v1, &v2);
         if (dg->edges[v1][v2] == 0)
             dg->edges[v1][v2] = 1;
-        printf("Enter the next group: ");
+        if (i != 9)
+            printf("Enter the next group: ");
     }
 
     return true;
@@ -47,10 +50,48 @@ bool CreateGraph(Digraph *dg)
 
 void PrintMatrix(Digraph *dg)
 {
+    /*打印邻接矩阵*/
     for (int r = 0; r < MAXVER; r++)
     {
         for (int c = 0; c < MAXVER; c++)
             printf("%-2d", dg->edges[r][c]);
         putchar('\n');
     }
+}
+
+int OutDegree(Digraph *dg, int verNo)
+{
+    /*出度数*/
+    int count = 0;
+
+    /*计算当前顶点的出度*/
+    for (int col = 0; col < MAXVER; col++)
+        if (dg->edges[verNo][col] == 1)
+            count++;
+
+    /*返回当前顶点的出度*/
+    return count;
+}
+
+int InDegree(Digraph *dg, int verNo)
+{
+    /*入度数*/
+    int count = 0;
+
+    /*计算当前顶点的入度*/
+    for (int row = 0; row < MAXVER; row++)
+        if (dg->edges[row][verNo] == 1)
+            count++;
+
+    /*返回当前顶点的入度*/
+    return count;
+}
+
+void ClearMatrix(Digraph *dg)
+{
+    /*将邻接矩阵全部置为0*/
+    for (int row = 0; row < MAXVER; row++)
+        for (int col = 0; col < MAXVER; col++)
+            if (dg->edges[row][col] == 1)
+                dg->edges[row][col] = 0;
 }
