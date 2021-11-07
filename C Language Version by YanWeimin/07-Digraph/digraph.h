ddd5433bb070a3bf179b56b94ece734e838d14ce
m66rk9t <m66rk9t@gmail.com>
2021-11-07 20:15:29 +0800

    update the definition of digraph

--- C Language Version by YanWeimin/07-Digraph/digraph.h
+++ C Language Version by YanWeimin/07-Digraph/digraph.h
@@ -24,10 +24,10 @@ typedef struct digraph
 /*前置条件：dg指向一个未初始化的有向图邻接矩阵*/
 /*后置条件：dg指向一个已初始化的有向图邻接矩阵*/
 bool CreateGraph(Digraph *dg);
-/*操作：打印无权有向图的邻接矩阵*/
-/*前置条件：dg指向一个已初始化的邻接矩阵*/
-/*后置条件：打印出dg指向的有向图的邻接矩阵*/
-void PrintMatrix(Digraph *dg);
+/*操作：清空邻接矩阵*/
+/*前置条件：dg指向一个已初始化的有向图邻接矩阵*/
+/*后置条件：将dg指向的有向图邻接矩阵置为全0*/
+void ClearMatrix(Digraph *dg);
 /*操作：求无权有向图中某顶点的出度*/
 /*前置条件：dg指向一个已初始化的有向图邻接矩阵*/
 /*        verNo是某顶点的编号*/
@@ -38,9 +38,9 @@ int OutDegree(Digraph *dg, int verNo);
 /*        verNo是某顶点的编号*/
 /*后置条件：计算并返回verNo的入度*/
 int InDegree(Digraph *dg, int verNo);
-/*操作：清空邻接矩阵*/
-/*前置条件：dg指向一个已初始化的有向图邻接矩阵*/
-/*后置条件：将dg指向的有向图邻接矩阵置为全0*/
-void ClearMatrix(Digraph *dg);
+/*操作：打印无权有向图的邻接矩阵*/
+/*前置条件：dg指向一个已初始化的邻接矩阵*/
+/*后置条件：打印出dg指向的有向图的邻接矩阵*/
+void PrintMatrix(Digraph *dg);
 
 #endif
