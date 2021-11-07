edbfc3904fd409d8c4795342de8ac2051155090e
m66rk9t <m66rk9t@gmail.com>
2021-11-07 19:54:31 +0800

    update the source file

--- C Language Version by YanWeimin/07-Digraph/usedigraph.c
+++ C Language Version by YanWeimin/07-Digraph/usedigraph.c
@@ -6,9 +6,14 @@
 int main(void)
 {
     Digraph dg;
+    int od = 0, id = 0;
     CreateGraph(&dg);
+    printf("The digraph has initialized.\n");
     printf("The martix of the digraph:\n");
     PrintMatrix(&dg);
+    od = OutDegree(&dg, 2);
+    id = InDegree(&dg, 2);
+    printf("Out degree:%d\nIn degree:%d\n", od, id);
 
     return 0;
 }
