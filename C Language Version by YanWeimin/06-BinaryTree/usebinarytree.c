edb81017d15c03c15d74b0b20c478807c2014c97
m66rk9t <m66rk9t@gmail.com>
2021-11-12 21:41:53 +0800

    update the source file

--- C Language Version by YanWeimin/06-BinaryTree/usebinarytree.c
+++ C Language Version by YanWeimin/06-BinaryTree/usebinarytree.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include "binarytree.h"
 
 int main(void)
 {
     BinaryTree myTree;
-    NodeType myNode = 1;
+    NodeType nodes[10] = {10, 5, 6, 9, 11, 2, 0, 1, 4, 3};
+    
+    InitTree(&myTree);//初始化二叉树
+    
+    for (int i = 0; i < 10; i++)//插入节点
+        AddNode(&myTree, &nodes[i]);
+    
+    /*先序、中序、后序遍历*/
+    PreOrder(&myTree, PrintNodeData);
+    putchar('\n');
+    InOrder(&myTree, PrintNodeData);
+    putchar('\n');
+    PostOrder(&myTree, PrintNodeData);
+    putchar('\n');
 
-    InitTree(&myTree);
-    AddNode(&myTree, &myNode);
-    myNode = 0;
-    AddNode(&myTree, &myNode);
-    myNode = 2;
-    AddNode(&myTree, &myNode);
-    printf("%d\n", myTree->ltree->ndata);
-    printf("%d\n", myTree->ndata);
-    printf("%d\n", myTree->rtree->ndata);
-    DestoryTree(&myTree);
+    DestoryTree(&myTree);//销毁二叉树
 
     return 0;
 }
