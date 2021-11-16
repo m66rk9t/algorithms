867132bd1e7190f665c525f0c1fb22e68a73bf84
m66rk9t <m66rk9t@gmail.com>
2021-11-16 13:52:28 +0800

    need to modify the source file

--- C Language Version by YanWeimin/07-Digraph(Adjacency List)/usedigraph.c
+++ C Language Version by YanWeimin/07-Digraph(Adjacency List)/usedigraph.c
@@ -3,16 +3,5 @@
 
 int main(void)
 {
-    AdjList myadj;
-
-    InitAdjList(&myadj);
-    
-    printf("%d %d\n", myadj.vers, myadj.arcs);
-    
-    for (int i = 0; i < myadj.vers; i++)
-        printf("%c", myadj.verlist[i].arctail);
-
-    DestoryAdjList(&myadj);
-
     return 0;
 }
